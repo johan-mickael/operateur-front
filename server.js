bdc4581129e0fddecbb2bd52b91f55d89883b106
johan <rakotoniainajohanmickael@gmail.com>
2021-04-02 11:35:44 +0300

    app name

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/s5-final-frontoffice'));
+app.use(express.static('./dist/s5-final'));
 
 app.get("/*", (req, res) =>
-    res.sendFile('index.html', { root: 'dist/s5-final-frontoffice/' }),
+    res.sendFile('index.html', { root: 'dist/s5-final/' }),
 );
 
 // Start the app by listening on the default Heroku port
